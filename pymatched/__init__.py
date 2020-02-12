64294636aa445a319799f993722e2680842c09f6
Seonghyeon Kim <k.seonghyeon@mymusictaste.com>
2020-02-12 17:59:46 +0900

    feat: Add pattern matching

--- pymatched/__init__.py
+++ pymatched/__init__.py
@@ -1,20 +1,48 @@
 from inspect import signature
-from typing import Mapping, Any, Callable
+from typing import Mapping, Any, Callable, Union
+
+
+class pattern:
+    def __init__(self, *args):
+        self.possible = tuple(args)
+
+    def __eq__(self, other):
+        return other in self.possible
+
+    def __contains__(self, item):
+        return item in self.possible
+
+    def __hash__(self):
+        return hash(self.possible)
+
 
 class match:
-    def __init__(self, func: Callable, *args: Any):
-        try:
-            self.v = func(*args)
-        except Exception as e:
-            self.v = e
+    def __init__(self, func: Union[Callable, Any], *args: Any):
+        if callable(func):
+            try:
+                self.v = func(*args)
+            except Exception as e:
+                self.v = e
+        else:
+            self.v = func
 
     def __rshift__(self, cases: Mapping[Any, Callable]) -> Any:
+        # pattern matching
+        for p, f in cases.items():
+            if type(p) == pattern and self.v in p:
+                if tuple(signature(f).parameters.keys()) == ():
+                    return f()
+                return f(self.v)
+
+        # basic matching
         f = cases.get(self.v) or cases.get(type(self.v))
+
         if f:
             if tuple(signature(f).parameters.keys()) == ():
                 return f()
             return f(self.v)
 
+        # handling default
         default = cases.get(...)
 
         if default:
@@ -22,16 +50,22 @@ class match:
                 return default()
             return default(self.v)
 
+        raise RuntimeError("match must handle every cases.")
+
 
 if __name__ == "__main__":
     def fx(x):
         # raise Exception("EXcepted")
         return x
 
-    x = match(fx, 3) >> {
-        int: lambda _: int,
-        str: lambda _: str,
+    v = fx(14)
+
+    # x = match(fx, 14) >> {
+    x = match(v) >> {
+        int: lambda v: print(f"{v} is int"),
+        str: lambda v: print(f"{v} is str"),
+        pattern(*[i for i in range(0, 10)]): lambda x: print("pattern 1 catched"),
+        pattern(*[i for i in range(10, 20)]): lambda x: print("pattern 2 catched"),
         Exception: lambda x: print(x),
         ...: lambda: print("default")
     }
-    print(f"type of x == {type(x)}")
