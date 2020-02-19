0f62b90a86f6073797fa57045f506cfacc9b30bb
Seonghyeon Kim <k.seonghyeon@mymusictaste.com>
2020-02-19 12:34:56 +0900

    feat: update to 0.0.2

--- pymatched/__init__.py
+++ pymatched/__init__.py
@@ -1,53 +1,130 @@
+import types
 from inspect import signature
 from typing import Mapping, Any, Callable, Union
+from uuid import uuid4
 
 
-class pattern:
-    def __init__(self, *args):
-        self.possible = tuple(args)
+def do(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        return e
+
+
+class PlaceholderType:
+    def __hash__(self):
+        return hash(uuid4().hex)
 
     def __eq__(self, other):
-        return other in self.possible
+        return True
+
+
+_ = PlaceholderType()
+
+
+class oneof:
+    def __init__(self, *args):
+        self.possibles = tuple(args)
+
+    def __eq__(self, other):
+        return other in self.possibles
 
     def __contains__(self, item):
-        return item in self.possible
+        return item in self.possibles
 
     def __hash__(self):
-        return hash(self.possible)
+        return hash(uuid4().hex)
 
 
 class match:
-    def __init__(self, func: Union[Callable, Any], *args: Any):
-        if callable(func):
-            try:
-                self.v = func(*args)
-            except Exception as e:
-                self.v = e
+    def __init__(self, target: Union[Callable, Any], *args: Any):
+        self.target = target
+
+    def __rshift__(self, cases: Mapping[Any, Any]) -> Any:
+        # if this is nested match
+        if (self.target is Ellipsis) or (self.target is _):
+            return lambda p: match(p) >> cases
+
+        # 1st order
+
+        try:
+            actn = cases.get(self.target)  # exact value match
+        except TypeError:  # raised when try to get with unhashable data
+            pass
         else:
-            self.v = func
+            if actn:
+                return self._do(actn)
 
-    def __rshift__(self, cases: Mapping[Any, Callable]) -> Any:
-        # pattern matching
-        for p, f in cases.items():
-            if type(p) == pattern and self.v in p:
-                if tuple(signature(f).parameters.keys()) == ():
-                    return f()
-                return f(self.v)
-
-        # basic matching
-        f = cases.get(self.v) or cases.get(type(self.v))
-
-        if f:
-            if tuple(signature(f).parameters.keys()) == ():
-                return f()
-            return f(self.v)
+        # 2nd order
+        for ptrn, actn in cases.items():
+            if self._match_oneof(ptrn):  # a | b | c => action
+                return self._do(actn)
+            if self._match_placeholder(ptrn):  # (a, _, c) => action
+                return self._do(actn)
+            if self._match_type_with_guard(ptrn):  # (<type>, <lambda x -> bool>) => action
+                return self._do(actn)
+        # 3rd order
+        actn = cases.get(type(self.target))  # <type> => action
+        if actn:
+            return self._do(actn)
 
         # handling default
-        default = cases.get(...)
+        default = cases.get(Ellipsis) or cases.get(Any)
 
         if default:
-            if tuple(signature(default).parameters.keys()) == ():
-                return default()
-            return default(self.v)
+            return self._do(default)
 
         raise RuntimeError("match must handle every cases.")
+
+    def _do(self, case):
+        if callable(case):
+            if tuple(signature(case).parameters.keys()) == ():
+                return case()
+            return case(self.target)
+        elif isinstance(case, tuple) or isinstance(case, list):
+            if len(case) >= 1 and callable(case[0]):
+                f = case[0]
+                args = (arg for arg in case[1:] if arg != _)
+                if _ in case:
+                    return f(self.target, *args)
+                else:
+                    return f(*args)
+        else:
+            return case
+
+    def _match_oneof(self, ptrn) -> bool:
+        is_match = False
+
+        if isinstance(ptrn, oneof) and (self.target in ptrn):
+            is_match = True
+
+        return is_match
+
+    def _match_placeholder(self, ptrn) -> bool:
+        return self.target == ptrn
+
+    # def _match_wildcard(self, ptrn, target):
+    #     if (not ptrn) and (not target):
+    #         return True
+    #
+    #     if (len(ptrn) > 1) and ptrn[0] == Ellipsis and (not target):
+    #         return False
+    #
+    #     if ((len(ptrn) > 1) and ptrn[0] == _) or (ptrn and target and ptrn[0] == target[0]):
+    #         return self._match_wildcard(ptrn[1:], target[1:])
+    #
+    #     if len(ptrn) != 0 and ptrn[0] == Ellipsis:
+    #         return self._match_wildcard(ptrn[1:], target) or self._match_wildcard(ptrn, target[1:])
+    #
+    #     return False
+
+    def _match_type_with_guard(self, ptrn) -> bool:
+        is_match = False
+
+        if isinstance(ptrn, tuple) and len(ptrn) ==2:
+            t = ptrn[0]
+            guard = ptrn[1]
+            if type(self.target) == t and isinstance(guard, types.LambdaType) and guard(self.target):
+                is_match = True
+
+        return is_match
